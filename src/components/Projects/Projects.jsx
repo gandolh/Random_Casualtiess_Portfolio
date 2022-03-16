a852995828194f047785a613dc792fa8e9404633
gandolh <gusatualex@yahoo.com>
2022-03-16 16:51:38 +0200

    rework on this

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RandomCasualtiess from './RandomCasualtiess';
+import GoNextButton from './GoNextButton';
+import WebsiteShowcaseRC from './WebsiteShowcaseRC';
+const Projects = () => {
+    return ( <>
+    <RandomCasualtiess/>
+    <GoNextButton/>
+    <WebsiteShowcaseRC/>
+    </> );
+}
+ 
+export default Projects;
