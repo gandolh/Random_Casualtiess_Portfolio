d0bdf89391d242b0bcc3d3aaeae98d7fdf5d9a11
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-02-02 23:49:08 +0200

    updated the script that renames static assets to a name urlfriendly

--- public/renameFiles.py
+++
@@ -1,7 +0,0 @@
-import os
-def replace(folder_path):
-    for (path, dirs, files) in os.walk(folder_path):
-        dst = path.replace(" ", "_")
-        os.rename(path, dst)
-
-replace('D:\projects\Random_Casualtiess_Portfolio\public\GenerativeArt')
