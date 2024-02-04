7cd455384ae3d05af1b2be8e55d486c5590a07ef
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-02-04 21:52:03 +0200

    updated base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from "path";
-import { fileURLToPath } from "node:url";
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Random_Casualtiess_Portfolio",
+  base: "/Random_Casualtiess_Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
