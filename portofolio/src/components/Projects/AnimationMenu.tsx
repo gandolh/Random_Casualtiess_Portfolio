3ca6454d71ecf9aa9e4fd55dd985f6885dc106d7
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-02-04 13:28:50 +0200

    implemented animations modals on project

--- portofolio/src/components/Projects/AnimationMenu.tsx
+++ portofolio/src/components/Projects/AnimationMenu.tsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom";
+import React from "react";
 import config from "@/config";
+import { Button } from "flowbite-react";
+import AnimationModal from "../modals/AnimationModal";
 const AnimationMenu = () => {
+    const [openModal, setOpenModal] = React.useState<boolean>(false);
+    const [animationPath, setAnimationPath] = React.useState<AnimationPath>({name: "", path: ""});
+
+    const handleOpenModal = (path: AnimationPath) => () => {
+        setOpenModal(true);
+        setAnimationPath(path);
+    };
     const animations = { ...config.AnimationPath };
 
     const buildMenu = (animations: AnimationPaths) => {
@@ -9,18 +18,22 @@ const AnimationMenu = () => {
         return (<div className="AnimationMenu">
             {(entries || []).map(([key, value], i) => {
                 return (
-                    <>
-                        <div className="InnerAnimationMenu" key={"InnerAnimationMenu" + i} >
-                            <p className="mytitleparagraph">{key}</p>
-                            {value.map((v, j) => (
-                                <Link key={"hrefAnimation" + j} to={v.path} >{v.name}</Link>
-                            ))}
-                        </div>
-                    </>
+                    <div className="InnerAnimationMenu" key={"InnerAnimationMenu" + i} >
+                        <h3 className="mytitleparagraph font-semibold"><center> {key}</center></h3>
+                        {value.map((v, j) => (
+                            <>
+                            <Button 
+                            size="sm" 
+                            color="gray"
+                            key={"hrefAnimation" + j} 
+                            onClick={handleOpenModal(v)}>{v.name}</Button>
+                            </>
+                        ))}
+                    </div>
                 )
-            }
-            )
-            }
+            })}
+
+            <AnimationModal animation={animationPath} openModal={openModal} setOpenModal={setOpenModal} />
         </div>)
     }
     return (
---
+++ portofolio/src/components/modals/AnimationModal.tsx
@@ -0,0 +1,32 @@
+import { Button, Modal } from "flowbite-react";
+type AnimationModalProps = {
+    animation : AnimationPath;
+    openModal: boolean;
+    setOpenModal: (open: boolean) => void;
+};
+
+const AnimationModal = ({animation, openModal, setOpenModal} : AnimationModalProps) => {
+    const prefix = `${window.location.origin}/GenerativeArt/`;
+
+    const getAnimUrl = (path: string) => {
+     return prefix + path.replace("\\","/") + "/index.html";
+    }
+    return (
+    <Modal show={openModal} onClose={() => setOpenModal(false)} size="7xl" >
+        <Modal.Header>{animation.name}</Modal.Header>
+        <Modal.Body >
+            <div className="space-y-6" style={{height: "60vh"}}>
+                <iframe src={getAnimUrl(animation.path)} width="100%" height="100%"></iframe> 
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+            <div className="flex justify-end space-x-4 w-full">
+            <Button onClick={() => setOpenModal(false)}  color="failure" outline size="md">Close</Button>
+            <Button onClick={() => setOpenModal(false)} size="md">Go full page</Button>
+            </div>
+        </Modal.Footer>
+    </Modal>
+    );
+}
+
+export default AnimationModal;
