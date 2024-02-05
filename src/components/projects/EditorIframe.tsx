93faa66b2ae435536cc5d6d7212d96f5fe822b32
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-02-05 16:33:42 +0200

    separated editor iframe in new component

---
+++ src/components/projects/EditorIframe.tsx
@@ -0,0 +1,17 @@
+type EditorIframeProps = {
+    srcPath: string,
+    uid: number
+}
+
+const EditorIframe = ({ srcPath, uid }: EditorIframeProps) => {
+    return (
+        <>
+            <div>
+                <iframe src={srcPath} width="100%" height="100%"></iframe>
+            </div>
+        </>
+
+    );
+}
+
+export default EditorIframe;
